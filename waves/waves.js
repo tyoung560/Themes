c94d59afff1f7e700b65d65ee70583e14b748822
tyoung560 <47755919+tyoung560@users.noreply.github.com>
2022-05-11 14:41:51 -0400

    TB.themeRender

--- waves/waves.js
+++ waves/waves.js
@@ -1,7 +1,9 @@
-var width = $(".x-menu-style-vertical").width();
-var offset = width + 0;
-if (window.matchMedia('(min-width: 768px)').matches) {
-  $(".app").css({
-    "margin-left": offset + "px"
-  });
-}
+TB.themeRender(function ($) {
+  var width = $(".x-menu-style-vertical").width();
+  var offset = width + 0;
+  if (window.matchMedia('(min-width: 768px)').matches) {
+    $(".app").css({
+      "margin-left": offset + "px"
+    });
+  }
+});
