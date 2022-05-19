4f8d1fd40a5fb11892c5f8b89c77fb690eed915b
tyoung560 <47755919+tyoung560@users.noreply.github.com>
2022-05-19 15:37:58 -0400

    menu fix

--- ghost/ghost.js
+++ ghost/ghost.js
@@ -1,9 +1,32 @@
-TB.themeRender(function ($) {
+TB.render('component_layout_3', function (data) {
+  var verticalNav = document.getElementsByClassName(
+    'x-menu-style-vertical'
+  ).length > 0;
+
   var width = $(".x-menu-style-vertical").width();
   var offset = width + 25;
-  if (window.matchMedia('(min-width: 768px)').matches) {
+
+  if (verticalNav) {
     $(".app").css({
       "margin-left": offset + "px"
     });
+    $(".navbar").css({
+      "overflow": "auto"
+    });
+    $(function () {
+      $('.dropdown-submenu').each(function () {
+        $(this).click(function () {
+          var dropdownContext = $(this).find('.dropdown-menu')[0];
+          if ($(dropdownContext).css('display') === 'contents') {
+            $(dropdownContext).css('display', 'none');
+          } else {
+            $(dropdownContext).css('display', 'contents');
+          }
+          event.stopPropagation();
+        });
+      });
+    });
+  } else {
+    console.log('horizontal menu');
   }
 });
