cbe9da221662fa700df227e779dcf84759697dd1
tyoung560 <47755919+tyoung560@users.noreply.github.com>
2022-05-19 22:02:13 -0400

    menu fixes

--- ghost/ghost.js
+++ ghost/ghost.js
@@ -1,18 +1,20 @@
 TB.themeRender(function($){
   var verticalNav = document.getElementsByClassName(
     'x-menu-style-vertical'
-  ).length > 0;
+  ).length > 0; // check if vertical menu class exists in DOM
 
-  var width = $(".x-menu-style-vertical").width();
-  var offset = width + 25;
+  var width = $(".x-menu-style-vertical").width(); // get vertical menu width
+  var offset = width + 25; //add pixels to veritcal menu width
 
   if (verticalNav) {
     $(".app").css({
       "margin-left": offset + "px"
-    });
+    }); // add margin to app class to shift app away from vertical menu
+
     $(".navbar").css({
       "overflow": "auto"
-    });
+    }); // add overflow to vertical menu to allow for menu scroll
+
     $(function () {
       $('.dropdown-submenu').each(function () {
         $(this).click(function () {
@@ -26,6 +28,7 @@ TB.themeRender(function($){
         });
       });
     });
+
   } else {
     console.log('horizontal menu');
   }
--- serenity/serenity.js
+++ serenity/serenity.js
@@ -1,9 +1,35 @@
 TB.themeRender(function($){
-  var width = $(".x-menu-style-vertical").width();
-  var offset = width + 0;
-  if (window.matchMedia('(min-width: 768px)').matches) {
+  var verticalNav = document.getElementsByClassName(
+    'x-menu-style-vertical'
+  ).length > 0; // check if vertical menu class exists in DOM
+
+  var width = $(".x-menu-style-vertical").width(); // get vertical menu width
+  var offset = width + 25; //add pixels to veritcal menu width
+
+  if (verticalNav) {
     $(".app").css({
       "margin-left": offset + "px"
+    }); // add margin to app class to shift app away from vertical menu
+
+    $(".navbar").css({
+      "overflow": "auto"
+    }); // add overflow to vertical menu to allow for menu scroll
+
+    $(function () {
+      $('.dropdown-submenu').each(function () {
+        $(this).click(function () {
+          var dropdownContext = $(this).find('.dropdown-menu')[0];
+          if ($(dropdownContext).css('display') === 'contents') {
+            $(dropdownContext).css('display', 'none');
+          } else {
+            $(dropdownContext).css('display', 'contents');
+          }
+          event.stopPropagation();
+        });
+      });
     });
+
+  } else {
+    console.log('horizontal menu');
   }
 });
