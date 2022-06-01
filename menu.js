0eb18f0b707087bb1e08fa395b09db6880ee1bd1
tyoung560 <47755919+tyoung560@users.noreply.github.com>
2022-06-01 17:08:33 -0400

    list row item margin

--- menu.js
+++ menu.js
@@ -1,30 +1,37 @@
 TB.themeRender(function ($) {
-  var width = $(".x-menu-style-vertical").width();
-  var offset = width + 25;
   if (window.matchMedia('(min-width: 768px)').matches) {
-    $(".app").css({
-      "margin-left": offset + "px"
-    });
+    var verticalNav = document.getElementsByClassName(
+      'x-menu-style-vertical'
+    ).length > 0; // check if vertical menu class exists in DOM
 
-    $(".navbar").css({
-      "overflow": "auto"
-    }); // add overflow to vertical menu to allow for menu scroll
+    var width = $(".x-menu-style-vertical").width(); // get vertical menu width
+    var offset = width + 25; //add pixels to veritcal menu width
 
-    $(function () {
-      $('.dropdown-submenu').each(function () {
-        $(this).click(function () {
-          var dropdownContext = $(this).find('.dropdown-menu')[0];
-          if ($(dropdownContext).css('display') === 'contents') {
-            $(dropdownContext).css('display', 'none');
-          } else {
-            $(dropdownContext).css('display', 'contents');
-          }
-          event.stopPropagation();
+    if (verticalNav) {
+      $(".app").css({
+        "margin-left": offset + "px"
+      }); // add margin to app class to shift app away from vertical menu
+
+      $(".navbar").css({
+        "overflow": "auto"
+      }); // add overflow to vertical menu to allow for menu scroll
+
+      $(function () {
+        $('.dropdown-submenu').each(function () {
+          $(this).click(function () {
+            var dropdownContext = $(this).find('.dropdown-menu')[0];
+            if ($(dropdownContext).css('display') === 'contents') {
+              $(dropdownContext).css('display', 'none');
+            } else {
+              $(dropdownContext).css('display', 'contents');
+            }
+            event.stopPropagation();
+          });
         });
       });
-    });
 
-  } else {
-    console.log('horizontal menu');
+    } else {
+      console.log('horizontal menu');
+    }
   }
 });
