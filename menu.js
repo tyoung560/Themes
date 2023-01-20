9a7c2726daed93eee8f1adf20fa8f54662846fa8
tyoung560 <47755919+tyoung560@users.noreply.github.com>
2023-01-20 09:17:13 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,40 +1,40 @@
-function checkAndUpdate(){
+function checkAndUpdate() {
   if (window.matchMedia('(min-width: 768px)').matches) {
-    var verticalNav = document.getElementsByClassName(
-      'x-menu-style-vertical'
-    ).length > 0; // check if vertical menu class exists in DOM
+      var verticalNav = document.getElementsByClassName(
+          'x-menu-style-vertical'
+      ).length > 0; // check if vertical menu class exists in DOM
 
-    var width = $(".x-menu-style-vertical").width(); // get vertical menu width
-    var offset = width + 25; //add pixels to veritcal menu width
+      var width = $(".x-menu-style-vertical").width(); // get vertical menu width
+      var offset = width + 25; //add pixels to veritcal menu width
 
-    if (verticalNav) {
-      $(".app").css({
-        "margin-left": offset + "px"
-      }); // add margin to app class to shift app away from vertical menu
+      if (verticalNav) {
+          $(".app").css({
+              "margin-left": offset + "px"
+          }); // add margin to app class to shift app away from vertical menu
 
-      $(".navbar").css({
-        "overflow": "auto"
-      }); // add overflow to vertical menu to allow for menu scroll
+          $(".navbar").css({
+              "overflow": "auto"
+          }); // add overflow to vertical menu to allow for menu scroll
 
-      $(function () {
-        $('.dropdown-submenu').each(function () {
-          $(this).click(function () {
-            var dropdownContext = $(this).find('.dropdown-menu')[0];
-            if ($(dropdownContext).css('display') === 'contents') {
-              $(dropdownContext).css('display', 'none');
-            } else {
-              $(dropdownContext).css('display', 'contents');
-            }
-            event.stopPropagation();
+          $(function () {
+              $('.dropdown-submenu').each(function () {
+                  $(this).click(function () {
+                      var dropdownContext = $(this).find('.dropdown-menu')[0];
+                      if ($(dropdownContext).css('display') === 'contents') {
+                          $(dropdownContext).css('display', 'none');
+                      } else {
+                          $(dropdownContext).css('display', 'contents');
+                      }
+                      event.stopPropagation();
+                  });
+              });
           });
-        });
-      });
 
-    } else {
-      $(".app").css({
-        "margin-left": "auto"
-      });
-    }
+      } else {
+          $(".app").css({
+              "margin-left": "auto"
+          });
+      }
   }
 }
 
@@ -42,20 +42,30 @@ TB.themeRender(function ($) {
   checkAndUpdate();
 });
 var observeUrlChange = () => {
-    var oldHref = document.location.href;
-    var body = document.querySelector("body");
-    var observer = new MutationObserver(mutations => {
+  var oldHref = document.location.href;
+  var body = document.querySelector("body");
+  var observer = new MutationObserver(mutations => {
       mutations.forEach(() => {
-        if (oldHref !== document.location.href) {
-          oldHref = document.location.href;
-          checkAndUpdate();
-        }
+          if (oldHref !== document.location.href) {
+              oldHref = document.location.href;
+              checkAndUpdate();
+          }
       });
-    });
-    observer.observe(body, {
+  });
+  observer.observe(body, {
       childList: true,
       subtree: true
-    });
-  };
+  });
+};
 
-  window.onload = observeUrlChange;
+window.onload = observeUrlChange;
+
+function listener() {
+  checkAndUpdate();
+}
+window.addEventListener('popstate', listener);
+
+var pushUrl = (href) => {
+  history.pushState({}, '', href);
+  window.dispatchEvent(new Event('popstate'));
+};
